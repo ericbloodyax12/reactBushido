68c29828df71dc7331368bcfa17d0638140a1072
Young Eric <erikbonopart12@gmail.com>
2022-12-28 23:10:29 +0300

    first commit: create first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { Compopnent } from 'react';
 import './App.css';
 
-function App() {
+const App = ()=> {
   return (
+      <div> first div
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Simple HTML
+      <ul>
+        <li> jsx </li>
+        <li> css </li>
+        <li> html </li>
+        <li> react </li>
+      </ul>
     </div>
+      </div>
   );
 }
 
