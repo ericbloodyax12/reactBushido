fa0eeda7d1341a146209882bb0e23563dc5a5b11
Young Eric <erikbonopart12@gmail.com>
2022-12-29 17:17:27 +0300

    Add exporting and importing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Compopnent } from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 const App = ()=> {
   return (
@@ -7,34 +9,8 @@ const App = ()=> {
           Simple HTML
           <Header />
           <Technologies />
-
-
-
-
       </div>
   );
-}
-const Header = ()=> {
-  return (
-      <div>
-        <a href="#$"> Home </a>
-        <a href="#$"> New feed </a>
-        <a href="#$"> Messages </a>
-      </div>
-  );
-}
-const Technologies = () => {
-    return(
-        <div>
-        <ul>
-            <li> jsx </li>
-            <li> css </li>
-            <li> html </li>
-            <li> react </li>
-        </ul>
-        </div>
-    )
-
 }
 
 export default App;
---
+++ src/Technologies.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Technologies = () => {
+    return(
+        <div>
+            <ul>
+                <li> jsx </li>
+                <li> css </li>
+                <li> html </li>
+                <li> react </li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Technologies;
