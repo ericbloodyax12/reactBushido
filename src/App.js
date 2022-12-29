65f2dac63fdda212a51fd5a0f60f5b529c7d8288
Young Eric <erikbonopart12@gmail.com>
2022-12-29 16:28:41 +0300

    Add new copmponents

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import './App.css';
 const App = ()=> {
   return (
       <div> first div
-    <div className="App">
-    Simple HTML
-      <ul>
-        <li> jsx </li>
-        <li> css </li>
-        <li> html </li>
-        <li> react </li>
-      </ul>
-    </div>
+          Simple HTML
+          <Header />
+          <Technologies />
+
+
+
+
       </div>
   );
+}
+const Header = ()=> {
+  return (
+      <div>
+        <a href="#$"> Home </a>
+        <a href="#$"> New feed </a>
+        <a href="#$"> Messages </a>
+      </div>
+  );
+}
+const Technologies = () => {
+    return(
+        <div>
+        <ul>
+            <li> jsx </li>
+            <li> css </li>
+            <li> html </li>
+            <li> react </li>
+        </ul>
+        </div>
+    )
+
 }
 
 export default App;
