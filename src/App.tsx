ec8ed54ae3f9185097429fbd6fc91b086a8f2896
Young Eric <erikbonopart12@gmail.com>
2023-04-28 23:19:05 +0300

    feat: -add header,ul and content -add grid style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app_wrapper">
+      <header className="app_header">
+          <img className={"app_header-img"}
+               src="https://techcrunch.com/wp-content/uploads/2016/11/icq-logo.png"
+          />
       </header>
+        <ul className={"app_ul"}>
+            <li><a>Profile</a></li>
+            <li><a>Friends</a></li>
+            <li><a>New</a></li>
+            <li><a>Settings</a></li>
+        </ul>
+      <div className={"app_content"}>
+          <div>
+              <img className={"app_content-img"}
+                    src={"https://www.worldbusinessculture.com/wp-content/uploads/2018/05/Canada-2.jpg"}
+              />
+          </div>
+          <div>
+              ava+ description
+          </div>
+          <div>
+              My posts
+              <div>
+                  New Post
+              </div>
+              <div>
+                  Post1
+              </div>
+          </div>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+
